19fa8bff35bf1eddd37b5e4c4ddc221f12e0e12e
Nabil Baadillah <nabil.baadillah@transdigital.co.id>
2020-05-03 20:59:46 +0700

    add styling fir the render display

---
+++ src/app/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export default makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    padding: 50,
+  },
+  pageTitle: {
+    marginBottom: 50,
+  },
+  gridList: {
+    width: '100%',
+    height: '100%',
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}))
