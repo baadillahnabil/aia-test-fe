e41267344e21c4149e7f2121656bfc589709f96c
Nabil Baadillah <nabil.baadillah@transdigital.co.id>
2020-05-03 19:34:28 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+
+const App = () => {
+  return (
+    <Container maxWidth='sm'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quibusdam odio suscipit
+        excepturi sit perspiciatis quae molestias non sapiente rerum, dolorem rem corporis officia
+        itaque voluptas, vitae delectus fuga ab?
+      </p>
+    </Container>
+  )
+}
+
+export default App
